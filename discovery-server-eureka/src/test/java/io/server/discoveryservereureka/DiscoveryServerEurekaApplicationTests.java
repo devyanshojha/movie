57b88589048bb1c530f97764ea08eee517dc7e06
devyanshojha <ojhadevyansh@gmail.com>
2021-06-06 06:16:16 +0530

    Initial commit

---
+++ discovery-server-eureka/src/test/java/io/server/discoveryservereureka/DiscoveryServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.server.discoveryservereureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
